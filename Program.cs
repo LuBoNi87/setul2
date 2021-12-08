eeb6a62dc9818f4ebfda05566459ff6c30f5b235
LuBoNi <luputbogdan87@yahoo.com>
2021-12-08 14:26:59 +0200

    problema 1

--- Program.cs
+++ Program.cs
@@ -8,32 +8,27 @@ namespace setul1
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
-            Console.WriteLine(@"1. Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare. 
-2. Rezolvati ecuatia de gradul 2 cu o necunoscuta: ax^2 + bx + c = 0, unde a, b si c sunt date de intrare. Tratati toate cazurile posibile. 
-3. Determinati daca n se divide cu k, unde n si k sunt date de intrare. 
-4. Detreminati daca un an y este an bisect. 
-5. Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga. 
-6. Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. 
-7. (Swap) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor. 
-8. (Swap restrictionat) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare.  
-9. Afisati toti divizorii numarului n. 
-10. Test de primalitate: determinati daca un numar n este prim.
-11. Afisati in ordine inversa cifrele unui numar n. 
-12. Determinati cate numere integi divizibile cu n se afla in intervalul [a, b]. 
-13. Determianti cati ani bisecti sunt intre anii y1 si y2.
-14. Determianti daca un numar n este palindrom. (un numar este palindrom daca citit invers obtinem un numar egal cu el, de ex. 121 sau 12321. 
-15. Se dau 3 numere. Sa se afiseze in ordine crescatoare. 
-16. Se dau 5 numere. Sa se afiseze in ordine crescatoare. (nu folositi tablouri)
-17. Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere. Folositi algoritmul lui Euclid. 
-18. Afisati descompunerea in factori primi ai unui numar n.  De ex. pentru n = 1176 afisati 2^3 x 3^1 x 7^2. 
-19. Determinati daca un numar e format doar cu 2 cifre care se pot repeta. De ex. 23222 sau 9009000 sunt astfel de numere, pe cand 593 si 4022 nu sunt. 
-20. Afisati fractia m/n in format zecimal, cu perioada intre paranteze (daca e cazul). Exemplu: 13/30 = 0.4(3). (https://github.com/HoreaOros/ROSE2020/blob/master/2611/Program.cs)
-        -O fractie este neperiodica daca numitorul este de forma 2^m*5^n unde m si n sunt mai mari sau egale decat 0
-        -O fractie este periodica simpla daca numitorul nu se divide cu 2 si nici cu 5. 
-        -O fractie este periodica mixta daca se divide cu 2 si/sau 5 SI se mai divide si cu alte numere prime diferite de 2 si 5. 
-21. Ghiciti un numar intre 1 si 1024 prin intrebari de forma ""numarul este mai mare sau egal decat x ? "". ");
+            Console.SetWindowSize(Console.LargestWindowWidth-10, Console.LargestWindowHeight-20);
+            Console.WriteLine(@"1.  Se da o secventa de n numere. Sa se determine cate din ele sunt pare. 
+2.  Se da o secventa de n numere. Sa se determina cate sunt negative, cate sunt zero si cate sunt pozitive.
+3.  Calculati suma si produsul numerelor de la 1 la n. 
+4.  Se da o secventa de n numere. Determinati pe ce pozitie se afla in secventa un numara a. Se considera ca primul element din secventa este pe pozitia zero. Daca numarul nu se afla in secventa raspunsul va fi -1. 
+5.  Cate elemente dintr-o secventa de n numere sunt egale cu pozitia pe care apar in secventa. Se considera ca primul element din secventa este pe pozitia 0. 
+6.  Se da o secventa de n numere. Sa se determine daca numerele din secventa sunt in ordine crescatoare. 
+7.  Se da o secventa de n numere. Sa se determine cea mai mare si cea mai mica valoare din secventa. 
+8.  Determianti al n-lea numar din sirul lui Fibonacci. Sirul lui Fibonacci se construieste astfel: f1 = 0, f2 = 1, f_n = f_(n-1) + f(n-2). Exemplu: 0, 1, 1, 2, 3, 5, 8 ...
+9.  Sa se determine daca o secventa de n numere este monotona. Secventa monotona = secventa monoton crescatoare sau monoton descrescatoare. 
+10. Se da o secventa de n numere. Care este numarul maxim de numere consecutive egale din secventa. 
+11. Se da o secventa de n numere. Se cere sa se caculeze suma inverselor acestor numere. 
+12. Cate grupuri de numere consecutive diferite de zero sunt intr-o secventa de n numere. Considerati fiecare astfel de grup ca fiind un cuvant, zero fiind delimitator de cuvinte. De ex. pentru secventa 1, 2, 0, 3, 4, 5, 0, 0, 6, 7, 0, 0 raspunsul este 3. 
+13. O <secventa crescatoare rotita> este o secventa de numere care este in ordine crescatoare sau poate fi transformata intr-o secventa in ordine crescatoare prin rotiri succesive (rotire cu o pozitie spre stanga = toate elementele se muta cu o pozitie spre stanga si primul element devine ultimul). Determinati daca o secventa de n numere este o secventa crescatoare rotita. 
+14. O <secventa monotona rotita> este o secventa de numere monotona sau poate fi transformata intr-o secventa montona prin rotiri succesive. Determinati daca o secventa de n numere este o secventa monotona rotita. 
+15. O secventa bitonica este o secventa de numere care incepe monoton crescator si continua monoton descrecator. De ex. 1,2,2,3,5,4,4,3 este o secventa bitonica. Se da o secventa de n numere. Sa se determine daca este bitonica. 
+16. O <secventa bitonica rotita> este o secventa bitonica sau una ca poate fi transformata intr-o secventa bitonica prin rotiri succesive (rotire = primul element devine ultimul). Se da o secventa de n numere. Se cere sa se determine daca este o secventa bitonica rotita. 
+17. Se da o secventa de 0 si 1, unde 0 inseamna paranteza deschisa si 1 inseamna paranteza inchisa. Determinati daca secventa reprezinta o secventa de paranteze corecta si,  daca este, determinati nivelul maxim de incuibare a parantezelor. De exemplu 0 1 0 0 1 0 1 1 este corecta si are nivelul maxim de incuibare 2 pe cand 0 0 1 1 1 0 este incorecta.");
+
             Console.WriteLine("Numarul problemei pe care vrei sa o rezolvi: ");
             int nrProblema = int.Parse(Console.ReadLine());
             switch (nrProblema)
@@ -60,87 +55,97 @@ namespace setul1
             }
         }
 
-        private static void Problema1()
+        public static void Problema1()
+        {
+            Console.WriteLine("Se da o secventa de n numere. Sa se determine cate din ele sunt pare.");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            while(n > 0)
+            {
+                int numar = int.Parse(Console.ReadLine());
+                if (numar % 2 == 0)
+                    count++;
+                n--;
+            }
+            Console.WriteLine($"In secventa sunt {count} numere pare");
+        }
+
+        public static void Problema2()
         {
             
         }
 
-        private static void Problema2()
+        public static void Problema3()
         {
             
         }
 
-        private static void Problema3()
+        public static void Problema4()
         {
             
         }
 
-        private static void Problema4()
+        public static void Problema5()
         {
             
         }
 
-        private static void Problema5()
+        public static void Problema6()
         {
             
         }
 
-        private static void Problema6()
+        public static void Problema7()
         {
             
         }
 
-        private static void Problema7()
+        public static void Problema8()
         {
             
         }
 
-        private static void Problema8()
+        public static void Problema9()
         {
             
         }
 
-        private static void Problema9()
+        public static void Problema10()
         {
             
         }
 
-        private static void Problema10()
+        public static void Problema11()
         {
             
         }
 
-        private static void Problema11()
+        public static void Problema12()
         {
             
         }
 
-        private static void Problema12()
+        public static void Problema13()
         {
             
         }
 
-        private static void Problema13()
+        public static void Problema14()
         {
             
         }
 
-        private static void Problema14()
+        public static void Problema15()
         {
             
         }
 
-        private static void Problema15()
+        public static void Problema16()
         {
             
         }
 
-        private static void Problema16()
-        {
-            
-        }
-
-        private static void Problema17()
+        public static void Problema17()
         {
             
         }
